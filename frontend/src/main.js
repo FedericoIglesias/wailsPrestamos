b2a545d28eca0800d8e40572e7182afb3227f593
FedericoIglesias <igl.fede@gmail.com>
2024-12-26 13:46:42 +0100

    featCheckFields

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -40,6 +40,74 @@ const createRowClient = (name, lastName, address, phone, email, dni, cuil, empre
   <td>${job}</td>
   </tr>`
 }
+const createRowPrestamo = (Amount, Interest, Cuota, Date, Client) => {
+  return `<tr>
+  <td>${Amount}</td>
+  <td>${Interest}</td>
+  <td>${Cuota}</td>
+  <td>${Date}</td>
+  <td>${Client}</td>
+  </tr>`
+}
+
+
+
+const createClient = () => {
+  const row = createRowClient(inptName.value, inptLastName.value, inptAddress.value, inptPhone.value, inptEmail.value, inptDNI.value, inptCUIL.value, inptEmpresa.value, inptJob.value)
+  tbodyClient.appendChild(document.createElement('tr')).innerHTML = row
+  inptName.value = ""
+  inptLastName.value = ""
+  inptAddress.value = ""
+  inptPhone.value = ""
+  inptEmail.value = ""
+  inptDNI.value = ""
+  inptCUIL.value = ""
+  inptEmpresa.value = ""
+  inptJob.value = ""
+}
+
+const createPrestamo = () => {
+  const row = createRowPrestamo(inputAmount.value, inputInterest.value, inputCuota.value, inputDate.value, selectClient.value)
+  tbodyPrestamo.appendChild(document.createElement('tr')).innerHTML = row
+  inputAmount.value = ''
+  inputInterest.value = ''
+  inputCuota.value = ''
+  inputDate.value = ''
+  selectClient.value = ''
+}
+
+const checkClient = () => {
+  let checkCount = 0
+
+  inptName.value == "" ? inptName.style.border = "1px solid red" : inptName.style.border = "1px solid black" & checkCount++
+  inptLastName.value == "" ? inptLastName.style.border = "1px solid red" : inptLastName.style.border = "1px solid black" & checkCount++
+  inptAddress.value == "" ? inptAddress.style.border = "1px solid red" : inptAddress.style.border = "1px solid black" & checkCount++
+  inptPhone.value == "" ? inptPhone.style.border = "1px solid red" : inptPhone.style.border = "1px solid black" & checkCount++
+  inptEmail.value == "" ? inptEmail.style.border = "1px solid red" : inptEmail.style.border = "1px solid black" & checkCount++
+  inptDNI.value == "" ? inptDNI.style.border = "1px solid red" : inptDNI.style.border = "1px solid black" & checkCount++
+  inptCUIL.value == "" ? inptCUIL.style.border = "1px solid red" : inptCUIL.style.border = "1px solid black" & checkCount++
+  inptEmpresa.value == "" ? inptEmpresa.style.border = "1px solid red" : inptEmpresa.style.border = "1px solid black" & checkCount++
+  inptJob.value == "" ? inptJob.style.border = "1px solid red" : inptJob.style.border = "1px solid black" & checkCount++
+
+  if (checkCount >= 9) {
+    return createClient()
+  }
+  return alert("Complete todos los campos")
+}
+
+const checkPrestamo = () => {
+  let checkCount = 0;
+  inputAmount.value == '' ? inputAmount.style.border = "1px solid red" : inputAmount.style.border = "1px solid black" & checkCount++
+  inputInterest.value == '' ? inputInterest.style.border = "1px solid red" : inputInterest.style.border = "1px solid black" & checkCount++
+  inputCuota.value == '' ? inputCuota.style.border = "1px solid red" : inputCuota.style.border = "1px solid black" & checkCount++
+  inputDate.value == '' ? inputDate.style.border = "1px solid red" : inputDate.style.border = "1px solid black" & checkCount++
+  selectClient.value == '' ? selectClient.style.border = "1px solid red" : selectClient.style.border = "1px solid black" & checkCount++
+  if (checkCount >= 5) {
+    console.log(checkCount)
+    return createPrestamo()
+  }
+  return alert("Complete todos los campos")
+}
 
 addClient.addEventListener("click", () => {
   sectionAddClient.style.display = "block";
@@ -67,24 +135,20 @@ tablePrestamo.addEventListener("click", () => {
 });
 
 btnPrestamo.addEventListener("click", () => {
-  console.log(inputAmount.value)
-  console.log(inputInterest.value)
-  console.log(inputCuota.value)
-  console.log(inputDate.value)
-  console.log(selectClient.value)
+  checkPrestamo()
 })
 
 btnClient.addEventListener("click", () => {
-  const row = createRowClient(inptName.value, inptLastName.value, inptAddress.value, inptPhone.value, inptEmail.value, inptDNI.value, inptCUIL.value, inptEmpresa.value, inptJob.value)
-  tbodyClient.appendChild(document.createElement('tr')).innerHTML = row
-  inptName.value= ""
-  inptLastName.value= ""
-  inptAddress.value= ""
-  inptPhone.value= ""
-  inptEmail.value= ""
-  inptDNI.value= ""
-  inptCUIL.value= ""
-  inptEmpresa.value= ""
-  inptJob.value= ""
+  checkClient()
 })
 
+sectionAddClient.addEventListener("keypress", (e) => {
+  if (e.key === 'Enter') {
+    checkClient()
+  }
+})
+sectionAddPrestamo.addEventListener("keypress", (e) => {
+  if (e.key === 'Enter') {
+    checkPrestamo()
+  }
+})
