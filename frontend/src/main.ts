c77fe7d517e94395c14280975ca2e796c26e4830
FedericoIglesias <igl.fede@gmail.com>
2024-12-29 15:36:19 +0100

    refactorCreatePrestamo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
-import { GetAllClient, SavePrestamo } from "../wailsjs/go/main/App.js";
 import { checkClient } from "./utils/createClient.js";
+import {
+  calculatePrestamo,
+  checkPrestamo,
+  fillSelectClient,
+  showCalculated,
+} from "./utils/createPrestamo.js";
 import { InitTableClient } from "./utils/tableClient.js";
 import { initTablePrestamo } from "./utils/tablePrestamo.js";
-import { Client, PrestamoBrought } from "./vite-env.js";
 const $ = (id: string) => document.getElementById(id);
 const $$ = (name: string) => document.querySelector(name);
 const addClient = $("add-client");
@@ -15,13 +19,6 @@ const sectionTableClient = $$(".table-client") as HTMLDivElement;
 const sectionTablePrestamo = $$(".table-prestamo") as HTMLDivElement;
 const btnPrestamo = $("btnPrestamo") as HTMLButtonElement;
 const btnClient = $("btnClient") as HTMLButtonElement;
-const inputAmount = $("inputAmount") as HTMLInputElement;
-const inputInterest = $("inputInterest") as HTMLInputElement;
-const inputCuota = $("inputCuota") as HTMLInputElement;
-const inputDate = $("inputDate") as HTMLInputElement;
-const selectClient = $("selectClient") as HTMLSelectElement;
-const descriptionPrestamo = $$(".descriptionPrestamo") as HTMLDivElement;
-const listClient = GetAllClient().then((data) => data);
 
 if (tablePrestamo)
   tablePrestamo.addEventListener("click", async () => {
@@ -31,47 +28,6 @@ if (tablePrestamo)
     sectionTableClient.style.display = "none";
     sectionTablePrestamo.style.display = "block";
   });
-
-const createPrestamo = () => {
-  const prestamo: PrestamoBrought = {
-    ID: "P" + new Date().getTime().toString(),
-    Amount: inputAmount.value,
-    Interest: inputInterest.value,
-    Cuota: inputCuota.value,
-    Date: new Date(inputDate.value).getTime().toString(),
-    ClientId: selectClient.value,
-  };
-  SavePrestamo(prestamo);
-
-  inputAmount.value = "";
-  inputInterest.value = "";
-  inputCuota.value = "";
-  inputDate.value = "";
-  selectClient.value = "";
-};
-
-const checkPrestamo = () => {
-  let checkCount = 0;
-  inputAmount.value == ""
-    ? (inputAmount.style.border = "1px solid red")
-    : ((inputAmount.style.border = "1px solid black"), checkCount++);
-  inputInterest.value == ""
-    ? (inputInterest.style.border = "1px solid red")
-    : ((inputInterest.style.border = "1px solid black"), checkCount++);
-  inputCuota.value == ""
-    ? (inputCuota.style.border = "1px solid red")
-    : ((inputCuota.style.border = "1px solid black"), checkCount++);
-  inputDate.value == ""
-    ? (inputDate.style.border = "1px solid red")
-    : ((inputDate.style.border = "1px solid black"), checkCount++);
-  selectClient.value == ""
-    ? (selectClient.style.border = "1px solid red")
-    : ((selectClient.style.border = "1px solid black"), checkCount++);
-  if (checkCount >= 5) {
-    return createPrestamo();
-  }
-  return alert("Complete todos los campos");
-};
 
 if (addClient)
   addClient.addEventListener("click", () => {
@@ -111,32 +67,6 @@ sectionAddPrestamo.addEventListener("change", () => {
   const { toPay } = calculatePrestamo();
   showCalculated(toPay);
 });
-
-const calculatePrestamo = () => {
-  const int = Number(inputInterest.value) * Number(inputCuota.value);
-  const toPay = Math.floor(Number(inputAmount.value) * (1 + int / 100));
-  return { int, toPay };
-};
-const showCalculated = (toPay: number) => {
-  const amountForMonth = Math.floor(toPay / Number(inputCuota.value));
-  const intForMonth = Math.floor(toPay - Number(inputAmount.value));
-  if (descriptionPrestamo)
-    return (descriptionPrestamo.innerHTML = `<div>
-    <p>* Monto a devolver: <span>${toPay}</span>$</p>
-    <p>* Monto por mes: <span>${amountForMonth}</span>$</p>
-    <p>* Monto de interes: <span>${intForMonth}</span>$</p>
-</div>`);
-  return;
-};
-
-const fillSelectClient = async () => {
-  (await listClient).map((client: Client) => {
-    let option: HTMLOptionElement = document.createElement("option");
-    option.value = client.ID;
-    option.text = client.DNI + "--" + client.Name + " " + client.Last_Name;
-    selectClient.add(option);
-  });
-};
 
 btnClient.addEventListener("click", () => {
   checkClient();
---
+++ frontend/src/utils/createPrestamo.ts
@@ -0,0 +1,77 @@
+import { GetAllClient, SavePrestamo } from "../../wailsjs/go/main/App.js";
+import { Client, PrestamoBrought } from "../vite-env.js";
+const $ = (id: string) => document.getElementById(id);
+const $$ = (name: string) => document.querySelector(name);
+const inputAmount = $("inputAmount") as HTMLInputElement;
+const inputInterest = $("inputInterest") as HTMLInputElement;
+const inputCuota = $("inputCuota") as HTMLInputElement;
+const inputDate = $("inputDate") as HTMLInputElement;
+const selectClient = $("selectClient") as HTMLSelectElement;
+const descriptionPrestamo = $$(".descriptionPrestamo") as HTMLDivElement;
+const listClient = GetAllClient().then((data) => data);
+
+const createPrestamo = () => {
+  const prestamo: PrestamoBrought = {
+    ID: "P" + new Date().getTime().toString(),
+    Amount: inputAmount.value,
+    Interest: inputInterest.value,
+    Cuota: inputCuota.value,
+    Date: new Date(inputDate.value).getTime().toString(),
+    ClientId: selectClient.value,
+  };
+  SavePrestamo(prestamo);
+
+  inputAmount.value = "";
+  inputInterest.value = "";
+  inputCuota.value = "";
+  inputDate.value = "";
+  selectClient.value = "";
+};
+
+export const checkPrestamo = () => {
+  let checkCount = 0;
+  inputAmount.value == ""
+    ? (inputAmount.style.border = "1px solid red")
+    : ((inputAmount.style.border = "1px solid black"), checkCount++);
+  inputInterest.value == ""
+    ? (inputInterest.style.border = "1px solid red")
+    : ((inputInterest.style.border = "1px solid black"), checkCount++);
+  inputCuota.value == ""
+    ? (inputCuota.style.border = "1px solid red")
+    : ((inputCuota.style.border = "1px solid black"), checkCount++);
+  inputDate.value == ""
+    ? (inputDate.style.border = "1px solid red")
+    : ((inputDate.style.border = "1px solid black"), checkCount++);
+  selectClient.value == ""
+    ? (selectClient.style.border = "1px solid red")
+    : ((selectClient.style.border = "1px solid black"), checkCount++);
+  if (checkCount >= 5) {
+    return createPrestamo();
+  }
+  return alert("Complete todos los campos");
+};
+export const calculatePrestamo = () => {
+  const int = Number(inputInterest.value) * Number(inputCuota.value);
+  const toPay = Math.floor(Number(inputAmount.value) * (1 + int / 100));
+  return { int, toPay };
+};
+export const showCalculated = (toPay: number) => {
+  const amountForMonth = Math.floor(toPay / Number(inputCuota.value));
+  const intForMonth = Math.floor(toPay - Number(inputAmount.value));
+  if (descriptionPrestamo)
+    return (descriptionPrestamo.innerHTML = `<div>
+    <p>* Monto a devolver: <span>${toPay}</span>$</p>
+    <p>* Monto por mes: <span>${amountForMonth}</span>$</p>
+    <p>* Monto de interes: <span>${intForMonth}</span>$</p>
+</div>`);
+  return;
+};
+
+export const fillSelectClient = async () => {
+  (await listClient).map((client: Client) => {
+    let option: HTMLOptionElement = document.createElement("option");
+    option.value = client.ID;
+    option.text = client.DNI + "--" + client.Name + " " + client.Last_Name;
+    selectClient.add(option);
+  });
+};
