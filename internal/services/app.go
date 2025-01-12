65af4a8d875bc6bae4511dab1b1256833983bf56
FedericoIglesias <igl.fede@gmail.com>
2025-01-13 00:14:18 +0100

    Revert "featTable"
    This reverts commit 33b2516f8458a5d75f24f9567d661d9cb020a97a.

--- internal/services/app.go
+++ internal/services/app.go
@@ -119,14 +119,13 @@ func FillDataPrestamo(listPrestamo []models.Prestamo, listClient []models.Client
 		for _, prestamo := range listPrestamo {
 			if prestamo.ClientId == client.ID {
 				prestamoTable := &models.PrestamoTable{
-					ID:             prestamo.ID,
-					PrestamoNumber: prestamo.PrestamoNumber,
-					Amount:         prestamo.Amount,
-					Interest:       prestamo.Interest,
-					Cuota:          prestamo.Cuota,
-					Date:           prestamo.Date,
-					ClientId:       prestamo.ClientId,
-					ClientName:     client.Name + " " + client.Last_Name,
+					ID:         prestamo.ID,
+					Amount:     prestamo.Amount,
+					Interest:   prestamo.Interest,
+					Cuota:      prestamo.Cuota,
+					Date:       prestamo.Date,
+					ClientId:   prestamo.ClientId,
+					ClientName: client.Name + " " + client.Last_Name,
 				}
 				listPrestamoTable = append(listPrestamoTable, *prestamoTable)
 			}
