d7bd9e8a696bec12515ca1b34199de37b32ce087
FedericoIglesias <igl.fede@gmail.com>
2025-01-21 23:10:09 +0100

    featRunAsAdmin

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"golang.org/x/sys/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
+	if !amAdmin() {
+		runMeElevated()
+	}
 	// Create an instance of the app structure
 	app := NewApp()
 	// Create application with options
@@ -30,4 +38,33 @@ func main() {
 	if err != nil {
 		println("Error:", err.Error())
 	}
+}
+
+func runMeElevated() {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func amAdmin() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+	if err != nil {
+		fmt.Println("admin no")
+		return false
+	}
+	fmt.Println("admin yes")
+	return true
 }
