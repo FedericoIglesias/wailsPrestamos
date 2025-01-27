6ac68191d0eb716955fb5e1dae754d6e445f5d58
FedericoIglesias <igl.fede@gmail.com>
2025-01-27 14:41:36 +0100

    lessPermission

--- main.go
+++ main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"os"
-	"strings"
-	"syscall"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"golang.org/x/sys/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	if !amAdmin() {
-		runMeElevated()
-	}
+	// if !amAdmin() {
+	// 	runMeElevated()
+	// }
 	// Create an instance of the app structure
 	app := NewApp()
 	// Create application with options
@@ -40,31 +34,31 @@ func main() {
 	}
 }
 
-func runMeElevated() {
-	verb := "runas"
-	exe, _ := os.Executable()
-	cwd, _ := os.Getwd()
-	args := strings.Join(os.Args[1:], " ")
+// func runMeElevated() {
+// 	verb := "runas"
+// 	exe, _ := os.Executable()
+// 	cwd, _ := os.Getwd()
+// 	args := strings.Join(os.Args[1:], " ")
 
-	verbPtr, _ := syscall.UTF16PtrFromString(verb)
-	exePtr, _ := syscall.UTF16PtrFromString(exe)
-	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
-	argPtr, _ := syscall.UTF16PtrFromString(args)
+// 	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+// 	exePtr, _ := syscall.UTF16PtrFromString(exe)
+// 	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+// 	argPtr, _ := syscall.UTF16PtrFromString(args)
 
-	var showCmd int32 = 1 //SW_NORMAL
+// 	var showCmd int32 = 1 //SW_NORMAL
 
-	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
+// 	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
 
-func amAdmin() bool {
-	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
-	if err != nil {
-		fmt.Println("admin no")
-		return false
-	}
-	fmt.Println("admin yes")
-	return true
-}
+// func amAdmin() bool {
+// 	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+// 	if err != nil {
+// 		fmt.Println("admin no")
+// 		return false
+// 	}
+// 	fmt.Println("admin yes")
+// 	return true
+// }
